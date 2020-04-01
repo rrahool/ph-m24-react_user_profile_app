872a29366b49eaf39ba5d4667d8d3eaf18f7cb85
rrahool <rbiswas596@gmail.com>
2020-04-01 13:11:16 +0600

    Deployed to Firebase (Without Firebase Auth)

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import List from './components/List/List';
+import * as firebase from "firebase/app";
+import firebaseConfig from './firebase.config';
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
+
   return (
     <div>
       <Header></Header>
